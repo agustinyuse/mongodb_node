fe25f7c17676348eb99e2c50086d0478b1dbb6ef
Agustin Yuse <agustinyuse@outlook.com>
2021-07-12 21:05:31 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mongodb_node
+
 Desafío coderhouse: MongoDB y node.js
+
+## Resúmen
+
+El desafío consta de entregar la misma funcionalidad de la clase 17 (SQLite 3) que tiene que ver a su vez con la clase "canal de chat" y su persistencia en base de datos relacionales.
+Para este punto la idea es usar mongoDB (base de datos NO relacional) para persistir los datos de mensajes y productos usando la dependencia mongoose para su interacción con nodejs.
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const config = require("./config/config.json");
+const dotenv = require("dotenv");
+
+require("./database/connection");
+
+dotenv.config();
+
+const http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+app.set("io", io);
+
+app.use(express.static("public"));
+
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  return res.status(500).send("Algo se rompio!");
+});
+
+const productsRouter = require("./routes/products.routes");
+app.use("/productos", productsRouter);
+
+const messagesRouter = require("./routes/messages.routes");
+app.use("/mensajes", messagesRouter);
+
+io.on("connect", (socket) => {
+  console.log("usuario conectado");
+});
+
+const PORT = process.env.PORT || config.PORT;
+
+http.listen(PORT, () => {
+  console.log(`servidor escuchando en http://localhost:${PORT}`);
+});
+
+http.on("error", (error) => {
+  console.log("error en el servidor:", error);
+});
